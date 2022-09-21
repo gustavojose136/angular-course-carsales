fb6ce57d21d8dd0ff658f7a8d2970d490112052c
Gustavo Jose <gjose2980@gmail.com>
2022-09-21 09:19:26 -0300

    yup

---
+++ src/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  privateKey: '2f639018-7c65-44df-a3a2-a070bd2a10a8',
+  // connectionUrlDatabase: 'mongodb://127.0.0.1:27017/car-sales',
+  connectionUrlDatabase: 'mongodb+srv://gustavo:Jack1982.@cluster0.aaxrtj1.mongodb.net/car-salesnp?retryWrites=true&w=majority',
+  apiPort: 8080,
+  corsOptions: {
+    origin: ['http://127.0.0.1:4200', 'http://localhost:4200'],
+  },
+};
